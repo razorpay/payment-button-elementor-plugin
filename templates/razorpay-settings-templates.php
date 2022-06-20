f18023381652d8e6e2297f49cc1e65a03fe61ba2
mudududla.sony <mudududla.sony@razorpay.com>
2022-06-20 13:01:25 +0530

    Add message after saving settings

--- templates/razorpay-settings-templates.php
+++ templates/razorpay-settings-templates.php
@@ -7,6 +7,8 @@ class RZP_Payment_Button_Elementor_Templates
     **/
     function razorpaySettings()
     {
+        settings_errors();
+        
         echo
             '<div class="wrap">
                 <h2>Razorpay Payment Button Settings</h2>
